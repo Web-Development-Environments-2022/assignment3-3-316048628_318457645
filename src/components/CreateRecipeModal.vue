0bbef9a2b42a1c6d3fbaeeaf4601daf97bfa0b25
navitbranin <navitbranin123@gmail.com>
2022-06-26 18:19:21 +0300

    create recipe Modal

--- src/components/CreateRecipeModal.vue
+++ src/components/CreateRecipeModal.vue
@@ -1,133 +1,155 @@
 <template>
-    <div class="container mt-5">
-        <div>
-            <b-button v-b-modal.modal-center>Create New Recipe</b-button>
-            <b-modal id="modal-center" centered title="Vue Bootstrap Centered Modal Example">
-                <div class="my-4">
-                    <h1 class="title">Register</h1>
-                    <b-form @submit.prevent="onRegister" @reset.prevent="onReset">
-                        <b-form-group id="input-group-username" label-cols-sm="3" label="Username:"
-                            label-for="username">
-                            <b-form-input id="username" v-model="$v.form.username.$model" type="text"
-                                :state="validateState('username')"></b-form-input>
-                            <b-form-invalid-feedback v-if="!$v.form.username.required">
-                                Username is required
-                            </b-form-invalid-feedback>
-                            <b-form-invalid-feedback v-else-if="!$v.form.username.length">
-                                Username length should be between 3-8 characters long
-                            </b-form-invalid-feedback>
-                            <b-form-invalid-feedback v-if="!$v.form.username.alpha">
-                                Username alpha
-                            </b-form-invalid-feedback>
-                        </b-form-group>
+  <div class="container mt-5">
+    <div>
+      <b-button v-b-modal.modal-center>Create New Recipe</b-button>
+      <b-modal id="modal-center" centered title="Vue Bootstrap Centered Modal Example">
+        <div class="my-4">
+          <h1 class="title">Register</h1>
+          <b-form @submit.prevent="onRegister" @reset.prevent="onReset">
+            <b-form-group id="input-group-title" label-cols-sm="3" label="Title:" label-for="title">
+              <b-form-input id="title" v-model="$v.form.title.$model" type="text" :state="validateState('title')">
+              </b-form-input>
+              <b-form-invalid-feedback v-if="!$v.form.title.required">
+                Title is required
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-else-if="!$v.form.title.length">
+                Title length should be at least 2 characters long
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-if="!$v.form.title.alpha">
+                Title alpha
+              </b-form-invalid-feedback>
+            </b-form-group>
 
-                        <b-form-group id="input-group-firstname" label-cols-sm="3" label="First name:"
-                            label-for="firstname">
-                            <b-form-input id="firstname" v-model="$v.form.firstname.$model" type="text"
-                                :state="validateState('firstname')"></b-form-input>
-                            <b-form-invalid-feedback v-if="!$v.form.firstname.required">
-                                First name is required
-                            </b-form-invalid-feedback>
-                            <b-form-invalid-feedback v-else-if="!$v.form.firstname.length">
-                                First name length should be at least 2 characters long
-                            </b-form-invalid-feedback>
-                            <b-form-invalid-feedback v-if="!$v.form.firstname.alpha">
-                                First name alpha
-                            </b-form-invalid-feedback>
-                        </b-form-group>
+            <b-form-group id="input-group-readyInMinutes" label-cols-sm="3" label="Preperation time in minutes:"
+              label-for="readyInMinutes">
+              <b-form-input id="readyInMinutes" v-model="$v.form.readyInMinutes.$model" type="text"
+                :state="validateState('readyInMinutes')"></b-form-input>
+              <b-form-invalid-feedback v-if="!$v.form.readyInMinutes.required">
+                Preperation time is required
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-if="!$v.form.readyInMinutes.numeric">
+                Preperation time is numeric
+              </b-form-invalid-feedback>
+            </b-form-group>
 
-                        <b-form-group id="input-group-lastname" label-cols-sm="3" label="Last name:"
-                            label-for="lastname">
-                            <b-form-input id="lastname" v-model="$v.form.lastname.$model" type="text"
-                                :state="validateState('lastname')"></b-form-input>
-                            <b-form-invalid-feedback v-if="!$v.form.lastname.required">
-                                Last name is required
-                            </b-form-invalid-feedback>
-                            <b-form-invalid-feedback v-else-if="!$v.form.lastname.length">
-                                Last name length should be at least 2 characters long
-                            </b-form-invalid-feedback>
-                            <b-form-invalid-feedback v-if="!$v.form.lastname.alpha">
-                                Last name alpha
-                            </b-form-invalid-feedback>
-                        </b-form-group>
+            <b-form-group id="input-group-imageUrl" label-cols-sm="3" label="Image URL:" label-for="imageUrl">
+              <b-form-input id="imageUrl" v-model="$v.form.imageUrl.$model" type="text"
+                :state="validateState('imageUrl')"></b-form-input>
+              <b-form-invalid-feedback v-if="!$v.form.imageUrl.required">
+                Image url is required
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-if="!$v.form.imageUrl.url">
+                Image Url should be url
+              </b-form-invalid-feedback>
+            </b-form-group>
 
-                        <b-form-group id="input-group-country" label-cols-sm="3" label="Country:" label-for="country">
-                            <b-form-select id="country" v-model="$v.form.country.$model" :options="countries"
-                                :state="validateState('country')"></b-form-select>
-                            <b-form-invalid-feedback>
-                                Country is required
-                            </b-form-invalid-feedback>
-                        </b-form-group>
+            <b-form-group id="input-group-popularity" label-cols-sm="3" label="Num of Likes:" label-for="popularity">
+              <b-form-input id="popularity" v-model="$v.form.popularity.$model" type="text"
+                :state="validateState('popularity')"></b-form-input>
+              <b-form-invalid-feedback v-if="!$v.form.popularity.required">
+                Popularity is required
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-if="!$v.form.popularity.numeric">
+                Popularity should be numeric
+              </b-form-invalid-feedback>
+            </b-form-group>
 
-                        <b-form-group id="input-group-Password" label-cols-sm="3" label="Password:"
-                            label-for="password">
-                            <b-form-input id="password" type="password" v-model="$v.form.password.$model"
-                                :state="validateState('password')"></b-form-input>
-                            <b-form-invalid-feedback v-if="!$v.form.password.required">
-                                Password is required
-                            </b-form-invalid-feedback>
-                            <b-form-text v-else-if="$v.form.password.$error" text-variant="info">
-                                Your password should be <strong>strong</strong>. <br />
-                                For that, your password should be also:
-                            </b-form-text>
-                            <b-form-invalid-feedback v-if="$v.form.password.required && !$v.form.password.length">
-                                Have length between 5-10 characters long
-                            </b-form-invalid-feedback>
-                        </b-form-group>
+            <b-form-group label="Vegan:" v-slot="{ ariaDescribedby }">
+              <b-form-radio id="vegan" v-model="selected" :aria-describedby="ariaDescribedby" name="some-radios"
+                value=true>
+              </b-form-radio>
+            </b-form-group>
 
-                        <b-form-group id="input-group-confirmedPassword" label-cols-sm="3" label="Confirm Password:"
-                            label-for="confirmedPassword">
-                            <b-form-input id="confirmedPassword" type="password"
-                                v-model="$v.form.confirmedPassword.$model" :state="validateState('confirmedPassword')">
-                            </b-form-input>
-                            <b-form-invalid-feedback v-if="!$v.form.confirmedPassword.required">
-                                Password confirmation is required
-                            </b-form-invalid-feedback>
-                            <b-form-invalid-feedback v-else-if="!$v.form.confirmedPassword.sameAsPassword">
-                                The confirmed password is not equal to the original password
-                            </b-form-invalid-feedback>
-                        </b-form-group>
+            <b-form-group label="Vegetarian:" v-slot="{ ariaDescribedby }">
+              <b-form-radio id="vegetarian" v-model="selected" :aria-describedby="ariaDescribedby" name="some-radios"
+                value=true>
+              </b-form-radio>
+            </b-form-group>
 
-                        <b-form-group id="input-group-email" label-cols-sm="3" label="Email:" label-for="email">
-                            <b-form-input id="email" type="text" v-model="$v.form.email.$model"
-                                :state="validateState('email')"></b-form-input>
-                            <b-form-invalid-feedback v-if="!$v.form.email.required">
-                                Email is required
-                            </b-form-invalid-feedback>
-                            <b-form-invalid-feedback v-else-if="!$v.form.email.email">
-                                The email is invalid
-                            </b-form-invalid-feedback>
-                        </b-form-group>
+            <b-form-group label="Gluten Free:" v-slot="{ ariaDescribedby }">
+              <b-form-radio id="glutenFree" v-model="selected" :aria-describedby="ariaDescribedby" name="some-radios"
+                value=true>
+              </b-form-radio>
+            </b-form-group>
 
-                        <b-button type="reset" variant="danger">Reset</b-button>
-                        <b-button type="submit" variant="primary" style="width:250px;" class="ml-5 w-75">Register
-                        </b-button>
-                        <div class="mt-2">
-                            You have an account already?
-                            <router-link to="login"> Log in here</router-link>
-                        </div>
-                    </b-form>
-                    <b-alert class="mt-2" v-if="form.submitError" variant="warning" dismissible show>
-                        Register failed: {{ form.submitError }}
-                    </b-alert>
-                    <!-- <b-card class="mt-3 md-3" header="Form Data Result">
+            <b-form-group id="input-group-ingredients" label-cols-sm="3" label="Ingredients:" label-for="ingredients">
+              <b-form-input id="ingredients" v-model="$v.form.ingredients.$model" type="text"
+                :state="validateState('ingredients')"></b-form-input>
+              <b-form-invalid-feedback v-if="!$v.form.ingredients.required">
+                ingredients is required
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-if="!$v.form.ingredients.alphaNum">
+                ingredients alphaNum
+              </b-form-invalid-feedback>
+            </b-form-group>
+
+            <b-form-group id="input-group-instructions" label-cols-sm="3" label="Instructions:" label-for="instructions">
+              <b-form-input id="instructions" v-model="$v.form.instructions.$model" type="text"
+                :state="validateState('instructions')"></b-form-input>
+              <b-form-invalid-feedback v-if="!$v.form.instructions.required">
+                instructions is required
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-if="!$v.form.instructions.alphaNum">
+                instructions alphaNum
+              </b-form-invalid-feedback>
+            </b-form-group>
+
+
+            <b-form-group id="input-group-numOfServings" label-cols-sm="3" label="Num of Servings:" label-for="numOfServings">
+              <b-form-input id="numOfServings" v-model="$v.form.popularity.$model" type="text"
+                :state="validateState('numOfServings')"></b-form-input>
+              <b-form-invalid-feedback v-if="!$v.form.numOfServings.required">
+                numOfServings is required
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-if="!$v.form.numOfServings.numeric">
+                numOfServings should be numeric
+              </b-form-invalid-feedback>
+            </b-form-group>
+
+
+            
+
+            <b-form-group id="input-group-email" label-cols-sm="3" label="Email:" label-for="email">
+              <b-form-input id="email" type="text" v-model="$v.form.email.$model" :state="validateState('email')">
+              </b-form-input>
+              <b-form-invalid-feedback v-if="!$v.form.email.required">
+                Email is required
+              </b-form-invalid-feedback>
+              <b-form-invalid-feedback v-else-if="!$v.form.email.email">
+                The email is invalid
+              </b-form-invalid-feedback>
+            </b-form-group>
+
+            <b-button type="reset" variant="danger">Reset</b-button>
+            <b-button type="submit" variant="primary" style="width:250px;" class="ml-5 w-75">Register
+            </b-button>
+            <div class="mt-2">
+              You have an account already?
+              <router-link to="login"> Log in here</router-link>
+            </div>
+          </b-form>
+          <b-alert class="mt-2" v-if="form.submitError" variant="warning" dismissible show>
+            Register failed: {{ form.submitError }}
+          </b-alert>
+          <!-- <b-card class="mt-3 md-3" header="Form Data Result">
       <pre class="m-0"><strong>form:</strong> {{ form }}</pre>
       <pre class="m-0"><strong>$v.form:</strong> {{ $v.form }}</pre>
     </b-card> -->
-                </div>
-            </b-modal>
         </div>
+      </b-modal>
     </div>
+  </div>
 </template>
 <script>
-import countries from "../assets/countries";
 import {
   required,
   minLength,
   maxLength,
   alpha,
-  sameAs,
-  email
+  numeric,
+  alphaNum,
+  email,
+  url
 } from "vuelidate/lib/validators";
 
 export default {
@@ -135,14 +157,16 @@ export default {
   data() {
     return {
       form: {
-        username: "",
-        firstname: "",
-        lastname: "",
-        country: null,
-        password: "",
-        confirmedPassword: "",
-        email: "",
-        submitError: undefined
+        title: "",
+        readyInMinutes: "",
+        imageUrl: "",
+        popularity: "",
+        vegan: false,
+        vegetarian: false,
+        glutenFree: false,
+        ingredients: "",
+        instructions: "",
+        numOfServings:""
       },
       countries: [{ value: null, text: "", disabled: true }],
       errors: [],
@@ -151,35 +175,34 @@ export default {
   },
   validations: {
     form: {
-      username: {
-        required,
-        length: (u) => minLength(3)(u) && maxLength(8)(u),
-        alpha
-      },
-      firstname: {
+      title: {
         required,
         length: (u) => minLength(2)(u),
         alpha
       },
-      lastname: {
+      readyInMinutes: {
         required,
-        length: (u) => minLength(2)(u),
-        alpha
+        numeric
       },
-      country: {
-        required
-      },
-      password: {
+      imageUrl: {
         required,
-        length: (p) => minLength(5)(p) && maxLength(10)(p)
+        url
       },
-      confirmedPassword: {
+      popularity: {
         required,
-        sameAsPassword: sameAs("password")
+        numeric
       },
-      email: {
+      ingredients: {
         required,
-        email
+        alphaNum
+      },
+      instructions: {
+        required,
+        alphaNum
+      },
+      numOfServings: {
+        required,
+        numeric
       }
     }
   },
@@ -200,16 +223,16 @@ export default {
           this.$root.store.server_domain + "/users/myRecipes",
 
           {
-            title: this.form.username,
-            readyInMinutes: this.form.firstname,
-            imageUrl: this.form.lastname,
-            popularity : this.form.country,
-            vegan: this.form.password,
-            vegetarian: this.form.email,
-            // glutenFree: ,
-            // ingredients: ,
-            // instructions: ,
-            // numOfServings
+            title: this.form.title,
+            readyInMinutes: this.form.readyInMinutes,
+            imageUrl: this.form.imageUrl,
+            popularity: this.form.popularity,
+            vegan: this.form.vegan,
+            vegetarian: this.form.vegetarian,
+            glutenFree: this.form.glutenFree,
+            ingredients: this.form.ingredients,
+            instructions: this.form.instructions,
+            numOfServings: this.form.numOfServings
           }
         );
         this.$router.push("/users/myRecipes");
