29de610f92321460a637361cc53d4daca307d356
TamarTzuberi <tamar.tzuberi1@gmail.com>
2022-06-29 15:52:10 +0300

    search fix bug and add new options

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -18,6 +18,25 @@
       >
       <b-form-select id="numOfRes" v-model="myNumOfRes" :options="options"></b-form-select>
       </b-form-group>
+       <b-form-group
+        id="input-group-diet"
+        label-cols-sm="3"
+        label="Diet:"
+        label-for="diet"
+      >
+      <b-form-select id="diet" v-model="myDiet" :options="dietOptions"></b-form-select>
+      </b-form-group>
+
+      
+       <b-form-group
+        id="input-group-intolerance"
+        label-cols-sm="3"
+        label="Intolerance:"
+        label-for="intolerance"
+      >
+      <b-form-select id="intolerance" v-model="myIntolerance" :options="intolerances"></b-form-select>
+      </b-form-group>
+
       <b-button type="reset" variant="danger">Reset</b-button>
       <b-button type="submit" variant="primary" style="width:250px;" class="ml-5 w-75">Search</b-button>
     </b-form>
@@ -25,9 +44,11 @@
 </template>
 
 <script>
+import dietOptions from "../assets/dietOptions";
+import intolerances from "../assets/intolerances";
+
 import {
   minLength,
-
 } from "vuelidate/lib/validators";
 
 export default {
@@ -37,17 +58,21 @@ export default {
       form: {
         queryS: "",
         numOfRes: 5,
-        cuisine: "",
+        cuisine: null,
         diet: "",
         intolerance: "",
         submitError: undefined
       },
+      dietOptions: [{ value: null, text: "", disabled: true }],
+      intolerances: [{ value: null, text: "", disabled: true }],
+
       // selected: 5,
       options: [
         { value: 5, text: '5 results' },
         { value: 10, text: '10 results' },
         { value: 15, text: '15 results' },
       ],
+
       errors: [],
       validated: false
     };
@@ -63,6 +88,29 @@ export default {
       }
     },
   },
+
+  myDiet: {get(){
+      return this.form.diet
+    },
+    set(value){
+      if(this.diet!=="")
+      {
+        this.form.diet = value
+      }
+    },
+  },
+
+myIntolerance: {get(){
+      return this.form.intolerance
+    },
+    set(value){
+      if(this.intolerance!=="")
+      {
+        this.form.intolerance = value
+      }
+    },
+  },
+
   },
   validations: {
     form: {
@@ -73,7 +121,8 @@ export default {
   },
   mounted() {
     // console.log("mounted");
-    // this.countries.push(...countries);
+    this.dietOptions.push(...dietOptions);
+    this.intolerances.push(...intolerances);
     // console.log($v);
   },
 
